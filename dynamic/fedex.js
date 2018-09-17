9f5432f40c9a5e8281cb2d7a642f3b4f331d871f
Sachin Patney <spatney@microsoft.com>
2018-09-16 20:16:23 -0700

    update

--- dynamic/fedex.js
+++ dynamic/fedex.js
@@ -1,24 +1,42 @@
+function print(items, goods) {
+    let str = '';
+    let count = 0;
+    for (const k in items) {
+        if (items[k] === 1) {
+            str += `${k},`
+            count += goods[parseInt(k)].weight;
+        }
+    }
+
+    console.log(count);
+    return str.substr(0, str.length - 1);
+}
+
 function computeMaxValue(goods, weightAllowed) {
-    const result = computeMaxValueInternal(goods, goods.length - 1, weightAllowed, 0, {});
+    const map = {};
+    const result = computeMaxValueInternal(goods, goods.length - 1, weightAllowed, 0, map);
     return result;
 }
 
-function computeMaxValueInternal(goods, i, weightRemaining, valueTotal, map) {
-    const key = `${i}-${weightRemaining}-${valueTotal}`;
+function computeMaxValueInternal(goods, i, capacityRemaining, valueTotal, map) {
+    const key = `${i}-${capacityRemaining}`;
     let result = map[key];
 
-    if (result !== undefined) return result;
-    if (i < 0) return { value: valueTotal, items: [] };
-    if (weightRemaining <= 0) return { value: valueTotal, items: [] };
+    if (result) return result;
+    if (i < 0 || capacityRemaining <= 0) return {
+        value: valueTotal,
+        items: {}
+    };
 
     const leftSubTree = computeMaxValueInternal(
         goods,
         i - 1,
-        weightRemaining,
+        capacityRemaining,
         valueTotal,
         map);
 
-    if ((weightRemaining - goods[i].weight) < 0) {
+    if (goods[i].weight > capacityRemaining) {
+        leftSubTree.items[i] = 0;
         map[key] = leftSubTree;
         return leftSubTree;
     }
@@ -26,44 +44,39 @@ function computeMaxValueInternal(goods, i, weightRemaining, valueTotal, map) {
     const rightSubTree = computeMaxValueInternal(
         goods,
         i - 1,
-        weightRemaining - goods[i].weight,
+        capacityRemaining - goods[i].weight,
         valueTotal + goods[i].value,
         map);
 
-    /*if (leftSubTree.value === rightSubTree.value) {
-        if (leftSubTree.items.length > rightSubTree.items.length) {
-            result = leftSubTree;
-        } else {
-            result = rightSubTree;
-        }
-    } else */if (leftSubTree.value > rightSubTree.value) {
+    if (leftSubTree.value > rightSubTree.value) {
         result = leftSubTree;
+        result.items[i] = 0;
+        map[key] = result; // not the most efficient.
     }
     else {
-        rightSubTree.items.push(i);
         result = rightSubTree;
+        result.items[i] = 1;
     }
-
-    map[key] = result;
 
     return result;
 }
 
 const goods = [
-    { value: 5, weight: 3 },
-    { value: 1, weight: 1 },
-    { value: 2, weight: 9 },
-    { value: 1, weight: 1 },
-    { value: 2, weight: 2 },
+    { value: 5, weight: 3 }, // 0
+    { value: 1, weight: 1 }, // 1
+    { value: 2, weight: 2 }, // 2
+    { value: 2, weight: 9 }, // 3
+    { value: 1, weight: 1 }, // 4
+    { value: 1, weight: 1 }, // 5
 ];
+
 const weightAllowed = process.argv.length > 1 ? process.argv[2] : 10;
 
-console.log("Weight allowed: \033[1;33;44m " + weightAllowed +  ' \033[0m');
+console.log("Weight allowed: \033[1;33;44m " + weightAllowed + ' \033[0m');
 
 const result = computeMaxValue(goods, weightAllowed);
-
 console.log(
     'Max value of the load is \033[1;33;44m '
     + result.value
     + ' \033[0m, and the item(s) included are '
-    + '\033[1;33;44m [ ' + result.items + ' ] \033[0m');
+    + '\033[1;33;44m [ ' + print(result.items, goods) + ' ] \033[0m');

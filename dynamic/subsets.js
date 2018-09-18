8b52f1a673a90001b88dfa02693aae2843eddb00
Sachin Patney <sachinp@qualtrics.com>
2018-09-18 10:50:01 -0700

    minor refactor

--- dynamic/subsets.js
+++ dynamic/subsets.js
@@ -1,17 +1,20 @@
 function countSubsets(numbers, sum) {
-    return countSubsetsInternal(numbers, sum, numbers.length - 1);
+    return countSubsetsInternal(numbers, sum, numbers.length - 1, {});
 }
 
-function countSubsetsInternal(numbers, sum, i) {
+function countSubsetsInternal(numbers, sum, i, map) {
     if (sum === 0) return 1;
     if (sum < 0 || i < 0) return 0;
 
-    const result = countSubsetsInternal(numbers, sum, i - 1) +
-        countSubsetsInternal(numbers, sum - numbers[i], i - 1);
+    const key = `${sum}-${i}`;
+    let result = map[key];
 
-    return result;
+    if (result) return result;
+
+    return map[key] = countSubsetsInternal(numbers, sum, i - 1, map) +
+        countSubsetsInternal(numbers, sum - numbers[i], i - 1, map);
 }
 
-const numbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
 console.log(countSubsets(numbers, parseInt(process.argv[2])));
